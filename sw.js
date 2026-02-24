d40b6c7106e1519fe85a6e87641cdee0f7115f43
Pavithra P <mailp2pavi@gmail.com>
2026-02-24 23:49:47 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,22 @@
 self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open("worker-app-v1").then((cache) =>
-      cache.addAll(["./", "./index.html", "./app.js", "./manifest.json"])
+    caches.open("worker-safety-pwa-v1").then((cache) =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./app.js",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png"
+      ].filter(Boolean))
     )
   );
 });
 
 self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  );
 });
 
 self.addEventListener("notificationclick", function(event) {
